cd6c3cb86e5ab6def532bcdb52aa8dc2f1283446
a-dabour <ah910905@gmail.com>
2023-10-08 08:38:20 -0700

    edit depoly pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,16 +18,16 @@ def do_deploy(archive_path):
             archive = archive_path.split("/")[1]
             put(archive_path, "/tmp/")
             run("mkdir -p /data/web_static/releases/{}/".format(fileName))
-            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-                archive, fileName))
+            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/\
+".format(archive, fileName))
             run("rm -rf /tmp/{}".format(archive))
-            run("mv /data/web_static/releases/{}/web_static/* "
-                "/data/web_static/releases/{}/".format(fileName, fileName))
-            run("rm -rf /data/web_static/releases/{}/web_static".format(
-                fileName))
+            run("mv /data/web_static/releases/{}/web_static/* \
+/data/web_static/releases/{}/".format(fileName, fileName))
+            run("rm -rf /data/web_static/releases/{}/web_static\
+".format(fileName))
             run("sudo rm -rf /data/web_static/current")
-            run("ln -s /data/web_static/releases/{} "
-                "/data/web_static/current".format(fileName))
+            run("ln -s /data/web_static/releases/{} \
+/data/web_static/current".format(fileName))
         except Exception:
             return False
         return True
