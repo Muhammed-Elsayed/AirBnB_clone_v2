d58d170e6ce7c1b796f61adeffc79cb2a2eecb06
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-10-21 19:06:04 +0300

    5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Flask framework"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home_page():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_page():
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_page(text):
+    """Displays c then the input text"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_page(text="is cool"):
+    """Displays python then the input text"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_int_page(n):
+    """display n if it's int"""
+    return "{} is a number".format(n)
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def html_page(n):
+    """html page display"""
+    return
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
